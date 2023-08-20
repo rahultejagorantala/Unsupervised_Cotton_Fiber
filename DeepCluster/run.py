80b92dfd119fc8181ba3a5a0623e87a080c62558
Rahul Teja Gorantala <101026703+rahultejagorantala@users.noreply.github.com>
2023-08-20 00:38:11 -0500

    Add files via upload

---
+++ DeepCluster/run.py
@@ -0,0 +1,8 @@
+run main.py [-h] [--arch ARCH] [--sobel] [--clustering {Kmeans,PIC}]
+               [--nmb_cluster NMB_CLUSTER] [--lr LR] [--wd WD]
+               [--reassign REASSIGN] [--workers WORKERS] [--epochs EPOCHS]
+               [--start_epoch START_EPOCH] [--batch BATCH]
+               [--momentum MOMENTUM] [--resume PATH]
+               [--checkpoints CHECKPOINTS] [--seed SEED] [--exp EXP]
+               [--verbose]
+               DIR
